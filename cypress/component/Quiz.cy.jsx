2c3eace0e98e7b262423a341a7a2e3cbd1c335c5
Gus <gus@macbookpro.lan>
2025-04-10 22:08:59 -0600

    completed E2E testing portion of test.

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -8,13 +8,13 @@ describe('<Quiz />', () => {
 
     it('should render the quiz start button', () => {
         cy.mount(<Quiz />)
-        cy.get('button').should('have.text', 'Start Quiz')
+        cy.get('[data-cy="start-button"').should('have.text', 'Start Quiz')
     })
 
     it('starts the quiz and displays "loading...", when "Start Quiz" button is clicked', () => {
         cy.mount(<Quiz />)
         cy.contains('Start Quiz').click()
-        cy.contains('Loading...')
+        cy.get('[data-cy="load-screen"]').should('have.text', 'Loading...')
     })
 
     it('fetches and displays a question and 4 selectable answers, after loading', () => {
@@ -28,26 +28,26 @@ describe('<Quiz />', () => {
         cy.wait('@fetchQuestion').its('response.statusCode').should('eq', 200);
 
         cy.get('h2').should('have.text', 'What is the output of print(2 ** 3)?')
-        cy.get('.d-flex').should('have.length', 4)
-        cy.get('.d-flex').first().within(() => {
-            cy.get('button').should('have.text', '1')
-            cy.get('.alert').should('have.text', '6')
+        cy.get('[data-cy="answers"]').should('have.length', 4)
+        cy.get('[data-cy="answers"]').first().within(() => {
+            cy.get('[data-cy="answer-selector"]').should('have.text', '1')
+            cy.get('[data-cy="answer"]').should('have.text', '6')
         })
-        cy.get('.d-flex').eq(1).within(() => {
-            cy.get('button').should('have.text', '2')
-            cy.get('.alert').should('have.text', '8')
+        cy.get('[data-cy="answers"]').eq(1).within(() => {
+            cy.get('[data-cy="answer-selector"]').should('have.text', '2')
+            cy.get('[data-cy="answer"]').should('have.text', '8')
         })
         cy.get('.d-flex').eq(2).within(() => {
-            cy.get('button').should('have.text', '3')
-            cy.get('.alert').should('have.text', '9')
+            cy.get('[data-cy="answer-selector"]').should('have.text', '3')
+            cy.get('[data-cy="answer"]').should('have.text', '9')
         })
         cy.get('.d-flex').eq(3).within(() => {
-            cy.get('button').should('have.text', '4')
-            cy.get('.alert').should('have.text', '12')
+            cy.get('[data-cy="answer-selector"]').should('have.text', '4')
+            cy.get('[data-cy="answer"]').should('have.text', '12')
         })
     })
 
-    it('verifies the correctness of the answer', () => {
+    it('shows another question when an answer is selected', () => {
         cy.intercept('GET','/api/questions/random', {
             fixture: 'questions.json'
         }).as('fetchQuestion')
@@ -57,13 +57,35 @@ describe('<Quiz />', () => {
 
         cy.wait('@fetchQuestion')
 
-        cy.get('.d-flex').eq(1).within(() => {
-            cy.get('button').click()
+        cy.get('[data-cy="answers"]').eq(1).within(() => {
+            cy.get('[data-cy="answer-selector"]').click()
         })
 
+        cy.get('h2').should('have.text', 'Which of the following is a mutable data type in Python?')
+        cy.get('[data-cy="answers"]').eq(3).within(() => {
+            cy.get('[data-cy="answer-selector"]').click()
+        })
+    })
+
+    it('shows the score when the final question is answered', () => {
+        cy.intercept('GET','/api/questions/random', {
+            fixture: 'questions.json'
+        }).as('fetchQuestion')
+
+        cy.mount(<Quiz />)
+        cy.contains('Start Quiz').click()
+
+        cy.wait('@fetchQuestion')
+
+        cy.get('[data-cy="answers"]').eq(1).within(() => {
+            cy.get('[data-cy="answer-selector"]').click()
+        })
+        cy.get('[data-cy="answers"]').eq(3).within(() => {
+            cy.get('[data-cy="answer-selector"]').click()
+        })
+        
         cy.get('h2').should('have.text', 'Quiz Completed')
-        cy.get('.alert').should('have.text', 'Your score: 1/1')
-        cy.get('button').should('have.text', 'Take New Quiz')
+        cy.get('[data-cy="score"]').should('have.text', 'Your score: 1/2')
     })
 
     it('starts the quiz again when "Take New Quiz" is selected', () => {
@@ -76,9 +98,13 @@ describe('<Quiz />', () => {
 
         cy.wait('@fetchQuestion')
 
-        cy.get('.d-flex').eq(1).within(() => {
-            cy.get('button').click()
+        cy.get('[data-cy="answers"]').eq(1).within(() => {
+            cy.get('[data-cy="answer-selector"]').click()
         })
+        cy.get('[data-cy="answers"]').eq(3).within(() => {
+            cy.get('[data-cy="answer-selector"]').click()
+        })
+
         cy.contains('Take New Quiz').click()
         cy.wait('@fetchQuestion').its('response.statusCode').should('eq', 200);
     })
